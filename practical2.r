c8da24e65bbfeb81550d85daaf0f9bcc47e079e5
Yannis <steelgiannis@gmail.com>
2022-10-12 12:27:39 +0100

    First file

---
+++ practical2.r
@@ -0,0 +1,14 @@
+
+##Practical 2: The prisoner problem simulation
+##Contributors: Yannis Stylianou (s2448736), Kartik (s2407270), Wang Chuhan()
+
+#-----------------------      Problem Description   ---------------------------#
+# 
+# 2n prisoners each have a unique prisoner number from 1 to 2n. The prison contains
+# a room in which there are 2n boxes, each with a unique number from 1 to 2n painted
+# on its lid. 2n cards, each printed with a unique number from 1 to 2n, are randomly
+# placed one in each box. The prisoners have the task of finding the card with their 
+# number on it by opening a maximum on n boxes. After each prisoner’s go, the room
+# is returned exactly to its original state and the prisoner is not allowed to
+# communicate with prisoners yet to have their go. If all prisoners succeed in
+# finding their number, then they all go free.
